a64f39c617698610b340a5408a0d97f5fce42c4f
Matheus Correia <matheuscorreia2005@gmail.com>
2019-07-29 22:48:50 -0300

    attempt continuous integration

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: 'production',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, '..', 'dist')
   },
   resolve: {
     extensions: [ '.js', '.ts', '.tsx' ],
