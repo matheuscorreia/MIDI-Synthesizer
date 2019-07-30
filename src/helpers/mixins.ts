d574d0a3a3cee4cf68cc70b5cc508ee4a74a5204
Matheus Correia <matheuscorreia2005@gmail.com>
2019-07-29 22:56:44 -0300

    fix visualizer grid css

--- src/helpers/mixins.ts
+++ src/helpers/mixins.ts
@@ -4,8 +4,7 @@ import { hex2rgba } from '../helpers/ui';
 
 export const backgroundGridMixin = css`
   background:
-  repeating-linear-gradient(90deg, ${props => hex2rgba(props.theme.colors.primary, 0.15)} 0.5px, transparent 0.5px),
-  repeating-linear-gradient(0deg, rgba(255,255,255, 0.15) 0.5px, transparent 0.5px);
-
+    linear-gradient(90deg, ${props => hex2rgba(props.theme.colors.primary, 0.15)} 0.5px, transparent 0.5px),
+    linear-gradient(0deg, ${props => hex2rgba(props.theme.colors.primary, 0.15)} 0.5px, transparent 0.5px);
   background-size: 5px 5px;
 `;
